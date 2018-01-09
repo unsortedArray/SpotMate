43c91500b42629f3fb0761994de5117bb97595f5
unknown <yashgulani333@gmail.com>
2018-01-09 22:44:07 +0530

    pehli commit

---
+++ js/loginPage.js
@@ -0,0 +1,30 @@
+function myFunction() {
+    var x = document.getElementById("login");
+    var y =document.getElementById("register");
+    console.log(x);
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        y.style.display = "none";
+    } else {
+        //x.style.display = "none";
+        //y.style.display = "block"
+    }
+    // y.style.display="none";
+
+}
+function myFunction2() {
+    var x = document.getElementById("register");
+    var y =document.getElementById("login");
+
+
+    console.log(x);
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        y.style.display = "none";
+    } else {
+        //x.style.display = "none";
+        //y.style.display = "block";
+    }
+    //y.style.display="none";
+
+}
---
+++ js/sample2.js
@@ -0,0 +1,1 @@
+var database = firebase.database();
